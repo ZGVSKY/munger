05deff3c72f83511d26e999cd7ae28d6e207ca13
zgvsk <zahaievskyi.oleksandr@chnu.edu.ua>
2025-11-06 23:25:00 +0200

    added simple player and world classes

---
+++ src/world/player.lua
@@ -0,0 +1,25 @@
+-------------------------------------------------------
+-- player class created at 07.11.25 by zgvsk
+-------------------------------------------------------
+
+player_class = {}
+
+function player_class:create(color)
+    local player = {}
+    player.color = color
+    player.gold = 0
+    player.wood = 0
+    player.stone = 0
+    player.is_died = false
+    player.caste = nil
+    player.buildings = {}
+    player.units = {}
+    return player
+
+function player_class:manage_resourses(player, gold, wood, stone)
+    player.gold  += gold
+    player.wood  += wood
+    player.stone += stone
+
+return player_class
+
