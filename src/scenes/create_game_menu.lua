7a80c1cce7c11c216e057c43e1a3290afd0439b7
A1k14 <sleeptaras@gmail.com>
2025-11-10 14:15:46 +0200

    implementation of adding a player icon and moving the plus sign to another position

--- src/scenes/create_game_menu.lua
+++ src/scenes/create_game_menu.lua
@@ -1,7 +1,3 @@
------------------------------------------------------------------------------------------
--- default placeholder
------------------------------------------------------------------------------------------
-
 -- import requirements
 composer = require( "composer" )
 scene = composer.newScene()
@@ -12,50 +8,32 @@ assets_path = 'src/assets/menu/'
 screen_width = display.actualContentWidth
 screen_height = display.actualContentHeight
 
+local players = {}
+local maxPlayers = 6
+local playersPerRow = 3
 
 function scene:create( event )
-    -- Called when the scene is created
-
     local sceneGroup = self.view
-    local phase = event.phase
-
-    -- preload assets as a textures
 
     local assest_to_load = {
-        'background (1).png',    
-        'logo.png',          
-        'Rectangle1.png',  
-        'Players.png', 
-        'Line1.png',         
-        'Line2.png',         
-        'player.png',        
-        'addnew.png'         
+        'background (1).png', 'logo.png', 'Rectangle1.png', 'Players.png', 
+        'Line1.png', 'Line2.png', 'player.png', 'addnew.png'         
     }
+
     
     local loaded_assets = {}
-
-   for i=1,#assest_to_load do
+    for i=1,#assest_to_load do
         loaded_assets[i] = graphics.newTexture( {
-            type = "image",
-            filename = assets_path .. assest_to_load[i],
+            type = "image", filename = assets_path .. assest_to_load[i],
             baseDir = system.ResourceDirectory
         } )
         loaded_assets[i]:preload()
-
-        if loaded_assets[i] == nil then
-            print( " Немає: " .. imagePath )
-
-        else
-            loaded_assets[i]:preload()
-        end
-
-        
     end
     sceneGroup.textures = loaded_assets
         
     local background = display.newImageRect( sceneGroup, sceneGroup.textures[1].filename, sceneGroup.textures[1].baseDir, screen_width, screen_height )
     local logo = display.newImageRect( sceneGroup, sceneGroup.textures[2].filename, sceneGroup.textures[2].baseDir, 338*2, 56*2 )
-    -- розмір панелі
+    -- щоб легше зробити розміри
     local panel_width = screen_width * 0.85
     local panel_height = screen_height * 0.65
 
@@ -63,65 +41,124 @@ function scene:create( event )
     local playersText = display.newImageRect( sceneGroup, sceneGroup.textures[4].filename, sceneGroup.textures[4].baseDir, 152*2, 43*2 )
     local line1 = display.newImageRect( sceneGroup, sceneGroup.textures[5].filename, sceneGroup.textures[5].baseDir, 442*2, 23*2 )
     local line2 = display.newImageRect( sceneGroup, sceneGroup.textures[6].filename, sceneGroup.textures[6].baseDir, 442*2, 23*2 )
-    local addButton = display.newImageRect( sceneGroup, sceneGroup.textures[8].filename, sceneGroup.textures[8].baseDir, 89*2, 60*2 )
 
+    background.x = screen_width / 2; background.y = screen_height / 2
+    logo.x = screen_width / 2; logo.y = screen_height * 0.15
+    panel.x = screen_width / 2; panel.y = screen_height * 0.55
+    playersText.x = screen_width / 2; playersText.y = panel.y - (panel_height * 0.1)
+    line1.x = screen_width / 2; line1.y = playersText.y + 70
+    line2.x = screen_width / 2; line2.y = panel.y + (panel_height * 0.25)
 
+    -- початкові координати плюса
+    local startX = (screen_width / 2) - 330
+    local startY = panel.y + (panel_height * 0.30)
+    
+    -- розміри і відступи іконок
+    local iconWidth = 89*2
+    local iconHeight = 60*2
+    local padding = 40 
+    local verticalPadding = 20 
+    
+    
+    -- Створює іконку гравця 
+    local function createPlayerIcon( xPos, yPos )
+        local icon = display.newImageRect( sceneGroup, sceneGroup.textures[7].filename, sceneGroup.textures[7].baseDir, iconWidth, iconHeight )
+        icon.x = xPos
+        icon.y = yPos
+        table.insert(players, icon) -- додає іконку в список
+        print("Створено іконку гравця. Всього гравців: " .. #players)
+        return icon
+    end
 
-    background.x = screen_width / 2
-    background.y = screen_height / 2
+    -- Створює кнопку "додати"
+    local function createAddButton( xPos, yPos )
+        local button = display.newImageRect( sceneGroup, sceneGroup.textures[8].filename, sceneGroup.textures[8].baseDir, iconWidth, iconHeight )
+        button.x = xPos
+        button.y = yPos
+        return button
+    end
 
-    logo.x = screen_width / 2
-    logo.y = screen_height * 0.15
+    -- коли натискаємо кнопку викликається ця функція
+    local function onAddButtonTap( event )
+        local currentButton = event.target 
 
-    panel.x = screen_width / 2
-    panel.y = screen_height * 0.55
+        if #players < maxPlayers then
+            print("Додаємо нового гравця...")
+            local newPlayerIcon = createPlayerIcon( currentButton.x, currentButton.y )
+            
+            -- Видаляємо кнопку плюс
+            currentButton.isVisible = false
+            currentButton:removeSelf()
+            currentButton = nil
 
-    playersText.x = screen_width / 2
-    playersText.y = panel.y - (panel_height * 0.1)
+            -- Перевіряємо, чи потрібно створювати нову кнопку плюс
+            if #players < maxPlayers then
+                
+                local nextButtonX
+                local nextButtonY
+                local currentPlayerCount = #players -- видає помилку якщо вказувати #players в if
 
-    line1.x = screen_width / 2
-    line1.y = playersText.y + 70
+                if (currentPlayerCount % playersPerRow == 0) then
+                    print("Досягли " .. currentPlayerCount .. " гравців, переносимо кнопку 'плюс' на новий рядок.")
+                    nextButtonX = startX
+                    nextButtonY = newPlayerIcon.y + iconHeight + verticalPadding 
+                else
+                    nextButtonX = newPlayerIcon.x + iconWidth + padding
+                    nextButtonY = newPlayerIcon.y 
+                end
 
-    line2.x = screen_width / 2
-    line2.y = panel.y + (panel_height * 0.25)
+                -- Створюємо НОВУ кнопку плюс
+                local newAddButton = createAddButton( nextButtonX, nextButtonY )
+                newAddButton:addEventListener( "tap", onAddButtonTap )
+                sceneGroup.currentAddButton = newAddButton 
 
-    addButton.x = (screen_width / 2) - 330
-    addButton.y =  panel.y + (panel_height * 0.30)
+            end
+            
+            print("Кількість гравців: " .. #players)
 
+        else
+        end
+        return true
+    end
+
+    local initialAddButton = createAddButton( startX, startY )
+    initialAddButton:addEventListener( "tap", onAddButtonTap ) -- оживляємо кнопку
+    sceneGroup.currentAddButton = initialAddButton
     
 end
 
 function scene:show( event )
     -- Called when the scene on the screen
-	local sceneGroup = self.view
-	local phase = event.phase
+    local sceneGroup = self.view
+    local phase = event.phase
     
     print( "show" )
 end
 
 function scene:hide( event )
-	local sceneGroup = self.view
-	local phase = event.phase
-	
-	if event.phase == "will" then
-		-- Called when the scene is on screen and is about to move off screen
-		-- e.g. stop timers, stop animation, unload sounds, etc.)
+    local sceneGroup = self.view
+    local phase = event.phase
+    
+    if event.phase == "will" then
+        -- Called when the scene is on screen and is about to move off screen
+        -- e.g. stop timers, stop animation, unload sounds, etc.)
 
-	elseif phase == "did" then
-		-- Called when the scene is now off screen
-	end	
+    elseif phase == "did" then
+        -- Called when the scene is now off screen
+    end 
 end
 
 function scene:destroy( event )
-	local sceneGroup = self.view
-	-- Called prior to the removal of scene's "view" (sceneGroup)
-	-- e.g. remove display objects, remove touch listeners, save state, etc.
+    local sceneGroup = self.view
+    -- Called prior to the removal of scene's "view" (sceneGroup)
+    -- e.g. remove display objects, remove touch listeners, save state, etc.
 
     -- unload all textures
     for i = 1,#sceneGroup.textures do
         sceneGroup.textures[i]:releaseSelf()
     end
-	display.remove(sceneGroup)
+    display.remove(sceneGroup)
+    players = {} -- Очищаємо список гравців при знищенні сцени
 end
 
 ---------------------------------------------------------------------------------
